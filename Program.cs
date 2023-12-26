ded3adf2dad4ecf93a16f4bdc62726c61e56fca7
Stanislav <stanich81@gmail.com>
2023-12-26 21:56:00 +0300

    добавили несколько задач по С шарп с лекции

--- Program.cs
+++ Program.cs
@@ -56,3 +56,88 @@
 // int n1 = n / 100;
 // int n3 = n % 10;
 // Console.WriteLine(n1 + n3);
+
+// int number = 18;
+// int i = 2;
+// while (i <= number)
+// {
+// Console.Write(i + "\t");
+// i=i+2;
+// }
+
+// int number = 12;
+// int i = 1;
+// while (i <= number)
+// {
+//     if ((i%2) == 0)
+//     {
+//         Console.Write(i + "\t");
+//     }
+// i++;
+// }
+
+// Console.Clear();
+// Console.WriteLine("Введите трёхзначное число : ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int a1 = a / 100;
+// int a2 = (a % 100) / 10;
+// int a3 = a % 10;
+
+// Console.WriteLine(a1 * 10 + a3);
+
+
+// Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+// Console.Clear();
+// Console.WriteLine("Введите трёхзначное число : ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int a1 = a / 100;
+// int a2 = (a % 100) / 10;
+// int a3 = a % 10;
+// Console.WriteLine($"{a1} в степени {a3} = {Math.Pow(a2, a3)}");
+
+
+//..Вывеси третью цифру слева от люого вводимого числа:
+// Console.Clear();
+// Console.WriteLine("Введите число : ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int copyA = a;
+// if (a <= 99)
+// {
+//     Console.WriteLine("Введеное число меньше трёхзначного или отрицательное");
+// }
+// else{
+// while (a >= 1000)
+// {
+//     a /= 10;
+// }
+// Console.WriteLine($"третья цифра слева от числа {copyA} = {(a % 10)}");
+// }
+
+
+// Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+Console.Clear();
+Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int copyA = a;
+if (a <= 99)
+{
+    Console.WriteLine("Введеное число меньше трёхзначного или отрицательное");
+}
+else
+{
+    a = a / 100 % 10;
+    Console.WriteLine($"третья цифра справа от числа {copyA} = {a}");
+}
